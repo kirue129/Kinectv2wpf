7a3b55a17b75764bbba4f40fe051ee5e3b9bdbb2
kirue129 <kirue129.develop@gmail.com>
2016-07-25 15:56:18 +0900

    カラー画像表示

--- Kinectv2wpf/MainWindow.xaml.cs
+++ Kinectv2wpf/MainWindow.xaml.cs
@@ -22,6 +22,18 @@ namespace Kinectv2wpf
     public partial class MainWindow : Window
     {
         KinectSensor kinect;
+
+        ColorFrameReader colorFrameReader;
+        FrameDescription colorFrameDesc;
+
+        ColorImageFormat colorFormat = ColorImageFormat.Bgra;
+
+        // WPF
+        WriteableBitmap colorBitmap;
+        byte[] colorBuffer;
+        int colorStride;
+        Int32Rect colorRect;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -31,13 +43,28 @@ namespace Kinectv2wpf
         {
             try
             {
+                // kinectを開く
                 kinect = KinectSensor.GetDefault();
                 if (kinect == null)
                 {
                     throw new Exception("Kinectを開けません");
                 }
-
                 kinect.Open();
+
+                // カラー画像の情報を作成する(BGRAフォーマット)
+                colorFrameDesc = kinect.ColorFrameSource.CreateFrameDescription(colorFormat);
+
+                // カラーリーダーを開く
+                colorFrameReader = kinect.ColorFrameSource.OpenReader();
+                colorFrameReader.FrameArrived += colorFrameReader_FrameArrived;
+
+                // カラー用のビットマップを作成する
+                colorBitmap = new WriteableBitmap(colorFrameDesc.Width, colorFrameDesc.Height, 96, 96, PixelFormats.Bgra32, null);
+                colorStride = colorFrameDesc.Width * (int)colorFrameDesc.BytesPerPixel;
+                colorRect = new Int32Rect(0, 0, colorFrameDesc.Width, colorFrameDesc.Height);
+                colorBuffer = new byte[colorStride * colorFrameDesc.Height];
+                ImageColor.Source = colorBitmap;
+
             }
             catch (Exception ex)
             {
@@ -46,8 +73,46 @@ namespace Kinectv2wpf
             }
         }
 
+        // 更新処理
+        void colorFrameReader_FrameArrived( object sender, ColorFrameArrivedEventArgs e)
+        {
+            UpdateColorFrame(e);
+            DrowColorFrame();
+        }
+
+        // 描画処理
+        private void UpdateColorFrame( ColorFrameArrivedEventArgs e)
+        {
+            // カラーフレームを取得する
+            using ( var colorFrame = e.FrameReference.AcquireFrame())
+            {
+                if (colorFrame == null)
+                {
+                    return;
+                }
+
+                // BGRAデータを取得
+                colorBuffer = new byte[colorFrameDesc.LengthInPixels * colorFrameDesc.BytesPerPixel];
+                colorFrame.CopyConvertedFrameDataToArray(colorBuffer, ColorImageFormat.Bgra);
+            }
+        }
+
+        private void DrowColorFrame()
+        {
+            // ビットマップする
+            colorBitmap.WritePixels(colorRect, colorBuffer, colorStride, 0);
+        }
+
+
+
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
+            if (colorFrameReader != null)
+            {
+                colorFrameReader.Dispose();
+                colorFrameReader = null;
+            }
+
             if (kinect != null)
             {
                 kinect.Close();
