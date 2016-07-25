35131318b7726169a0af6795a38f9b9be8dda9de
kirue129 <kirue129.develop@gmail.com>
2016-07-25 18:57:55 +0900

    赤外線画像

--- Kinectv2wpf/MainWindow.xaml.cs
+++ Kinectv2wpf/MainWindow.xaml.cs
@@ -24,17 +24,15 @@ namespace Kinectv2wpf
         // Kinect SDK
         KinectSensor kinect;
 
-        DepthFrameReader depthFrameReader;
-        FrameDescription depthFrameDesc;
+        InfraredFrameReader infraredFrameReader;
+        FrameDescription infraredFrameDesc;
 
         // 表示用
-        WriteableBitmap depthImage;
-        ushort[] depthBuffer;
-        byte[] depthBitmapBuffer;
-        Int32Rect depthRect;
-        int depthStride;
+        WriteableBitmap infraredBitmap;
+        ushort[] infraredBuffer;
+        Int32Rect infraredRect;
+        int infraredStride;
 
-        Point depthPoint;
         const int R = 20;
 
         public MainWindow()
@@ -54,24 +52,21 @@ namespace Kinectv2wpf
                 }
                 kinect.Open();
 
-                // 表示のためのデータを作成
-                depthFrameDesc = kinect.DepthFrameSource.FrameDescription;
+                // 赤外線画像リーダーを取得する
+                infraredFrameDesc = kinect.InfraredFrameSource.FrameDescription;
 
-                // Depthリーダーを開く
-                depthFrameReader = kinect.DepthFrameSource.OpenReader();
-                depthFrameReader.FrameArrived += depthFrameReader_FrameArrived;
+                // 赤外線リーダーを開く
+                infraredFrameReader = kinect.InfraredFrameSource.OpenReader();
+                infraredFrameReader.FrameArrived += infraredFrameReader_FrameArrived;
 
                 // 表示のためにビットマップに必要なものを作成
-                depthImage = new WriteableBitmap(depthFrameDesc.Width, depthFrameDesc.Height, 96, 96, PixelFormats.Gray8, null);
-                depthBuffer = new ushort[depthFrameDesc.LengthInPixels];
-                depthBitmapBuffer = new byte[depthFrameDesc.LengthInPixels];
-                depthRect = new Int32Rect(0, 0, depthFrameDesc.Width, depthFrameDesc.Height);
-                depthStride = (int)(depthFrameDesc.Width);
+                infraredBuffer = new ushort[infraredFrameDesc.LengthInPixels];
+                infraredBitmap = new WriteableBitmap(infraredFrameDesc.Width, infraredFrameDesc.Height, 96, 96, PixelFormats.Gray16, null);
+                infraredRect = new Int32Rect(0, 0, infraredFrameDesc.Width, infraredFrameDesc.Height);
+                infraredStride = infraredFrameDesc.Width * (int)infraredFrameDesc.BytesPerPixel;
 
-                ImageDepth.Source = depthImage;
-
-                // 初期の位置表示座標(中心点)
-                depthPoint = new Point(depthFrameDesc.Width / 2, depthFrameDesc.Height / 2);
+                ImageColor.Source = infraredBitmap;
+                
 
             }
             catch (Exception ex)
@@ -82,48 +77,40 @@ namespace Kinectv2wpf
         }
 
         // 更新処理
-        void depthFrameReader_FrameArrived(object sender, DepthFrameArrivedEventArgs e)
+        void infraredFrameReader_FrameArrived(object sender, InfraredFrameArrivedEventArgs e)
         {
-            UpdateDepthFrame(e);
-            DrowDepthFrame();
+            UpdateInfraredFrame(e);
+            DrowInfraredFrame();
         }
 
-        private void UpdateDepthFrame( DepthFrameArrivedEventArgs e)
+        private void UpdateInfraredFrame( InfraredFrameArrivedEventArgs e)
         {
-            using ( var depthFrame = e.FrameReference.AcquireFrame())
+            // からあーフレームを取得する
+            using ( var infraredFrame = e.FrameReference.AcquireFrame())
             {
-                if (depthFrame == null)
+                if (infraredFrame == null)
                 {
                     return;
                 }
 
-                // Depthデータを取得
-                depthFrame.CopyFrameDataToArray(depthBuffer);
+                // 赤外線データを取得
+                infraredFrame.CopyFrameDataToArray(infraredBuffer);
             }
         }
 
-        private void DrowDepthFrame()
+        private void DrowInfraredFrame()
         {
-            // 距離情報の表示を更新する
-            UpdateDepthValue();
-
-            // 0~8000のデータを0~65535のデータに変換する(見やすく)
-            for(int i = 0 ; i < depthBuffer.Length ; i++)
-            {
-                depthBitmapBuffer[i] = (byte)(depthBuffer[i] % 255);
-            }
-
-            depthImage.WritePixels(depthRect, depthBuffer, depthStride, 0);            
+            infraredBitmap.WritePixels(infraredRect, infraredBuffer, infraredStride, 0);            
         }
 
 
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            if ( depthFrameReader != null)
+            if ( infraredFrameReader != null)
             {
-                depthFrameReader.Dispose();
-                depthFrameReader = null;
+                infraredFrameReader.Dispose();
+                infraredFrameReader = null;
             }
 
             if (kinect != null)
@@ -133,43 +120,5 @@ namespace Kinectv2wpf
             }
         }
 
-        // クリックした座標を取得
-        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            depthPoint = e.GetPosition(this);
-        }
-
-        // クリックした座標の距離を表示
-        private void UpdateDepthValue()
-        {
-            CanvasPoint.Children.Clear();
-
-            // クリックしたポイントを表示する
-            var ellipse = new Ellipse()
-            {
-                Width = R,
-                Height = R,
-                StrokeThickness = R / 4,
-                Stroke = Brushes.Red,
-            };
-            Canvas.SetLeft(ellipse, depthPoint.X - (R / 2));
-            Canvas.SetTop(ellipse, depthPoint.Y - (R / 2));
-            CanvasPoint.Children.Add(ellipse);
-
-            // クリックしたポイントのインデックスを計算する
-            int depthindex = (int)((depthPoint.Y * depthFrameDesc.Width) + depthPoint.X);
-
-            // クリックしたポインタの距離を表示する
-            var text = new TextBlock()
-            {
-                Text = string.Format("{0}mm", depthBuffer[depthindex]),
-                FontSize = 20,
-                Foreground = Brushes.Green,
-            };
-            Canvas.SetLeft(text, depthPoint.X);
-            Canvas.SetTop(text, depthPoint.Y - R);
-            CanvasPoint.Children.Add(text);
-
-        }
     }
 }
